59f1a174dd7a6f8ca9a3241d974c3da98dc6b0be
Youssef Sherif <sharief@aucegypt.edu>
2018-01-13 22:18:57 +0200

    Refactor "donation places" query

--- src/data-layer/read-places.js
+++ src/data-layer/read-places.js
@@ -1,15 +1,19 @@
 const placeModel = require('../models/donation-place.model')
 
 module.exports = {
-    getPlaces({ limit, skip, searchFilter, categoryFilter, cityFilter }) {
-        const query = { category: categoryFilter, city: cityFilter }
+    getPlaces({ limit=10, skip = 0, searchFilter, categoryFilter, cityFilter }) {
+        const query = { }
         if (searchFilter) query.name = { $regex: RegExp(`.*${searchFilter}.*`) }
-        return placeModel.find(query).limit(limit).skip(skip).lean().exec()
+        if (categoryFilter) query.category = categoryFilter
+        if (cityFilter) query.city = cityFilter
+        return placeModel.find(query).limit(limit).skip(parseInt(skip)).lean().exec()
     },
 
-    getPlacesCount(searchFilter, categoryFilter, cityFilter) {
-        const query = { category: categoryFilter, city: cityFilter }
+    getPlacesCount({searchFilter, categoryFilter, cityFilter}) {
+        const query = { }
         if (searchFilter) query.name = { $regex: RegExp(`.*${searchFilter}.*`) }
+        if (categoryFilter) query.category = categoryFilter
+        if (cityFilter) query.city = cityFilter
         return placeModel.find(query).count().lean().exec()
     },
 }
--- src/routes/get-places.js
+++ src/routes/get-places.js
@@ -1,10 +1,8 @@
 const db = require('../data-layer/read-places')
 
 module.exports = (req, res, next) => {
-        console.log(req.query)
     let promise
-    const query = { limit: 10, skip: parseInt(req.query.skip), searchFilter: req.query.searchFilter, categoryFilter: req.query.categoryFilter, cityFilter: req.query.cityFilter }
-    promise = Promise.all([db.getPlaces(query), db.getPlacesCount(req.query.searchFilter, req.query.categoryFilter, req.query.cityFilter)])
+    promise = Promise.all([db.getPlaces(req.query), db.getPlacesCount(req.query)])
     return promise.then(([places, count]) => {
         res.status(200).json({ places, count })
     }).catch(err => next(err))
